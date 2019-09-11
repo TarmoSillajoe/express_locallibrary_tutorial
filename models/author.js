f3e890910ae255f4cfb3dd18bff820f94d48e8d4
Tarmo <tsillajoe@gmail.com>
2019-09-11 06:19:20 +0300

    model for author created

---
+++ models/author.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+  first_name: { type: String, required: true, max: 100 },
+  family_name: { type: String, required: true, max: 100 },
+  date_of_birth: {type: Date},
+  date_of_death: {type: Date},
+  }
+);
+
+// Virtual for autohr's full name
+AuthorSchema
+.virtual('name')
+.get(function() {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function() {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear());
+});
+
+// Virtual for author's url
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this.id;
+});
+
+// Export model
+module.exports = mongoose.model('Author',AuthorSchema);
