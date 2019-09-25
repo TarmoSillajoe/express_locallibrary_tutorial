cae25e09ee3ebba2b8ae17a405ae5525e3b9781b
Tarmo <tsillajoe@gmail.com>
2019-09-25 07:02:44 +0300

    created controller modules in folder /controllers

---
+++ controllers/genreController.js
@@ -0,0 +1,40 @@
+var genre = require('../models/genre');
+
+//GET genres
+exports.genre_list = function (req,res) {
+  res.send('NOT IMPLEMENTED: all genres list');
+};
+
+exports.genre_detail = function (req,res) {
+  res.send(`NOT IMPLEMENTED: genre id ${req.params.id} details`);
+};
+
+// Display Genre create form on GET.
+exports.genre_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre create GET');
+};
+
+// Handle Genre create on POST.
+exports.genre_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre create POST');
+};
+
+// Display Genre delete form on GET.
+exports.genre_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre delete GET');
+};
+
+// Handle Genre delete on POST.
+exports.genre_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre delete POST');
+};
+
+// Display Genre update form on GET.
+exports.genre_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre update GET');
+};
+
+// Handle Genre update on POST.
+exports.genre_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre update POST');
+};
