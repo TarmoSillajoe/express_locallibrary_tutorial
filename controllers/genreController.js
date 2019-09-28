c90056db124412aa35d43823e76f51665ff7ee42
Tarmo <tsillajoe@gmail.com>
2019-09-28 07:07:06 +0300

    added template for creating a genre "genre_form.pug"

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,65 +1,73 @@
 const validator = require("express-validator");
 var Genre = require('../models/genre');
 
-//GET genres
-exports.genre_list = function (req,res) {
-  res.send('NOT IMPLEMENTED: all genres list');
+// Display list of all Genre.
+exports.genre_list = function(req, res) {
+  res.send("NOT IMPLEMENTED: Genre list");
 };
 
-exports.genre_detail = function (req,res) {
-    res.render('genre_form', {title: 'Create Genre'});
+// Display detail page for a specific Genre.
+exports.genre_detail = function(req, res) {
+  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
-    res.render('genre-form', {title:  "Create Genre"});
+exports.genre_create_get = function(req, res, next) {
+    res.render('genre_form', {title:  "Create Genre"});
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = [
-    // Validate that the name field is not empty.
-    validator.body('name','Genre name required').isLength({ min: 1 }).trim(),
+exports.genre_create_post =  [
 
-    // Sanitize (escape) the name field.
+    // Validate that the name field is not empty.
+    validator.body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+
+  // Sanitize (escape) the name field.
     validator.sanitizeBody('name').escape(),
 
-    // Process request after validation and sanitization.
+  // Process request after validation and sanitization.
     (req, res, next) => {
-        // Extract validation errors from request
-        const errors = validator.validationResult(req);
 
-        //Create a genre object with escaped and trimmed data
-        var genre = new Genre(
-            { name: req.body.name }
-        );
+    // Extract the validation errors from a request.
+    const errors = validator.validationResult(req);
 
-        if (!errors.isEmpty()) {
-            res.render('genre-form', {title: 'Create genre', genre: genre, errors: errors.array()});
-            return;
-        }
-        else {
-            // Data form is valid
-            //Check if genre with the same name already exists
-            Genre.findOne({'name': req.body.name})
-            .exec( function(err, found_genre) {
-                if (err) {return next(err);}
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre(
+        { name: req.body.name }
+    );
 
-                if (found_genre) {
-                    //Genre exists. Redirect to its detail page
-                    res.redirect(found_genre.url):
-                }
-                else {
-                    genre.save(function(err) {
-                        if (err) {return next(err); }
-                        //Genre saved. Redirect to genre detail page
-                        res.redirect(genre.url);
-                    });
-                }
 
-            });
-        }
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+        res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+        return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+        Genre.findOne({ 'name': req.body.name })
+        .exec( function(err, found_genre) {
+            if (err) { return next(err); }
+
+            if (found_genre) {
+             // Genre exists, redirect to its detail page.
+                res.redirect(found_genre.url);
+            }
+            else {
+
+                genre.save(function (err) {
+                    if (err) { return next(err); }
+                    // Genre saved. Redirect to genre detail page.
+                    res.redirect(genre.url);
+                });
+
+            }
+
+        });
+    }
     }
 ];
+
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
